8d6e494a80db46391c382f9ec51521c78a3d68e1
Ayush-raj99 <mk4shivam@gmail.com>
2026-08-01 16:47:55 +0000

    Restore chat page CSS and update UI

--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -1,5 +1,6 @@
 """
 RoomChat V2
+
 Home Routes
 """
 
@@ -11,23 +12,41 @@ from app.models.models import Room
 from app.templates_engine import templates
 
 
+
 router = APIRouter(
     tags=["Home"]
 )
 
 
-@router.api_route("/", methods=["GET", "HEAD"])
+
+
+
+@router.api_route(
+    "/",
+    methods=["GET", "HEAD"]
+)
 def home(
     request: Request,
     db: Session = Depends(get_db)
 ):
 
+
     rooms = db.query(Room).all()
 
+
+
     return templates.TemplateResponse(
+
         request=request,
+
         name="index.html",
+
         context={
+
+            "request": request,
+
             "rooms": rooms
+
         }
+
     )
