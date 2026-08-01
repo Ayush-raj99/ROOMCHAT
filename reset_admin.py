5b0e632f99147b05c71481ecf91f28ad3e66dfd4
Ayush-raj99 <mk4shivam@gmail.com>
2026-08-01 12:24:10 +0000

    Fixed main indentation error

--- reset_admin.py
+++ reset_admin.py
@@ -6,12 +6,12 @@ from app.services.security import hash_password
 db = SessionLocal()
 
 admin = db.query(User).filter(
-    User.username == "admin"
+    User.username == "Ayush"
 ).first()
 
 
 if admin:
-    admin.password = hash_password("NewPassword123")
+    admin.password = hash_password("PJNU6S5A")
     db.commit()
     print("Admin password changed")
 else:
